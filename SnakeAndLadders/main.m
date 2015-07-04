0993c2b70882b7231a608df18d81d65c6acdddf8
rcacho <rcacho@hotmail.ca>
2015-07-04 12:59:35 -0700

    Initial Commit

---
+++ SnakeAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakeAndLadders
+//
+//  Created by ricardo antonio cacho on 2015-07-04.
+//  Copyright (c) 2015 ricardo antonio cacho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
